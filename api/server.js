2ed42216420b4f01c941d849045b1487f48a2c90
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-14 23:48:11 -0300

    firts comit

---
+++ api/server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+
+const mercadopago = require("mercadopago");
+
+const clave = process.env.ACCESS_TOKEN;
+console.log(clave);
+
+mercadopago.configure({
+  access_token: clave,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../client")));
+app.use(cors());
+
+app.get("/", function (req, res) {
+  const filePath = path.resolve(__dirname, "..", "client", "index.html");
+  res.sendFile(filePath);
+});
+
+app.post("/create_preference", (req, res) => {
+  let preference = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.quantity),
+      },
+    ],
+    back_urls: {
+      success: "http://localhost:8080",
+      failure: "http://localhost:8080",
+      pending: "",
+    },
+    auto_return: "approved",
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.json({
+        id: response.body.id,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.get("/feedback", function (req, res) {
+  res.json({
+    Payment: req.query.payment_id,
+    Status: req.query.status,
+    MerchantOrder: req.query.merchant_order_id,
+  });
+});
+
+app.listen(8080, () => {
+  console.log("The server is now running on Port 8080");
+});
